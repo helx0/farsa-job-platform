8daacbc159c6298ca8ec68f22447668479a8a552
helx0 <eng.ayhabtwfyq@gmail.com>
2026-09-19 01:32:21 +0300

    security: restrict PHP API CORS and error exposure

--- php/api/reviews.php
+++ php/api/reviews.php
@@ -1,15 +1,7 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 require_once '../config.php';
+header('Content-Type: application/json; charset=utf-8');
+configureCors();
 require_once '../includes/functions.php';
 require_once '../includes/reviews.php';
 
