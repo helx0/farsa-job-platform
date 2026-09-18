9f3b0d66a12397784ad2fce358e9dc24726622af
helx0 <eng.ayhabtwfyq@gmail.com>
2026-09-19 01:32:10 +0300

    security: restrict PHP API CORS and error exposure

--- php/api/auth.php
+++ php/api/auth.php
@@ -1,18 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 try {
     require_once '../config.php';
+header('Content-Type: application/json; charset=utf-8');
+configureCors();
     require_once '../includes/functions.php';
 } catch (Exception $e) {
     http_response_code(500);
